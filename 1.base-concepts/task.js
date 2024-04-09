af4ea41bcc80dff8e560d712d1d50369dcc6d4c6
Ksenia <ksenyat98@mail.ru>
2024-04-09 19:52:51 +0300

    Готовое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,27 +1,28 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let d = (b**2-4*a*c);
-  let arr = [d];
-  if (arr<0) {
-    console.log ([' ']);
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    return arr;
   }
-    else if (arr=0) {
-      let answer = (-b/(2*a));
-      console.log ([answer]);
+    else if (d === 0) {
+      let x = -b / (2 * a);
+      arr.push(x);
     }
-      else if (arr>0) {
-        let firstAnswer = ((-b + Math.sqrt(d) )/(2*a));
-        let secondAnswer = ((-b - Math.sqrt(d) )/(2*a));
-        console.log ([firstAnswer + secondAnswer]);
-
+      else {
+        let x1 = (-b + Math.sqrt(d)) / (2 * a);
+        let x2 = (-b - Math.sqrt(d)) / (2 * a);
+        arr.push(x1, x2);
       }
-
   return arr;
 }
-
-console.log (solveEquation (-4, 0, -7));
-
+console.log (solveEquation(3, 4, 6));
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let p = percent / 100 / 12;
+  let payment = amount - contribution;
+  let monthlyPayment = payment * (p + (p / (((1 + p) ** countMonths) - 1)));
+  let totalAmount = monthlyPayment * countMonths;
+  return +totalAmount.toFixed(2);
 }
+console.log (calculateTotalMortgage(10, 0, 50000, 12));
